71c4518c18e6b5ed3fd8009e07928dfb7a4d6ea1
Alexander Martynenko <MartynenkoAS@yandex.ru>
2023-10-14 18:14:58 +0300

    добавлен слайдер фото в Completed projects

---
+++ index.js
@@ -0,0 +1,79 @@
+
+const completedProjects = [
+{
+img:  "images/Large_home_twochair.png",
+city: "Rostov-on-Don LCD admiral",
+area: "81 m2",
+time: "3.5 months"
+},{
+img:  "images/Large_home_sochi.png",
+city: "Sochi Thieves",
+area: "105 m2",
+time: "4 months"
+},{
+img:  "images/Large_home_rostov_patriotic.png",
+city: "Rostov-on-Don Patriotic",
+area: "93 m2",
+time: "3 months"
+}
+]
+
+let index = 0;
+const img  = document.querySelector(".main__secondpart_image_img");
+const city = document.querySelector(".main__secondpart_info-data_element_city");
+const area = document.querySelector(".main__secondpart_info-data_element_area");
+const time = document.querySelector(".main__secondpart_info-data_element_time");
+
+const ringButton_0  = document.querySelector(".main__secondpart_info-data_ring_button_0");
+const ringButton_1  = document.querySelector(".main__secondpart_info-data_ring_button_1");
+const ringButton_2  = document.querySelector(".main__secondpart_info-data_ring_button_2");
+const menuElement_0 = document.querySelector(".main__secondpart_image-menu-element_0");
+const menuElement_1 = document.querySelector(".main__secondpart_image-menu-element_1");
+const menuElement_2 = document.querySelector(".main__secondpart_image-menu-element_2");
+
+function funcClick(i) {
+    img.src        = completedProjects[i].img;
+    city.innerText = completedProjects[i].city;
+    area.innerText = completedProjects[i].area;
+    time.innerText = completedProjects[i].time;
+    
+    switch (i) {
+        case 0:
+            ringButton_0.style.fillOpacity = "1";
+            ringButton_1.style.fillOpacity = "0.3";
+            ringButton_2.style.fillOpacity = "0.3";
+            menuElement_0.style.color = "#E3B873";
+            menuElement_1.style.color = "rgba(255, 255, 255, 0.3)";
+            menuElement_2.style.color = "rgba(255, 255, 255, 0.3)";
+            break;                                      //вопрос: без break работало не правильно. заходило во все case
+        case 1:
+            ringButton_0.style.fillOpacity = "0.3";
+            ringButton_1.style.fillOpacity = "1";
+            ringButton_2.style.fillOpacity = "0.3";
+            menuElement_0.style.color = "rgba(255, 255, 255, 0.3)";
+            menuElement_1.style.color = "#E3B873";
+            menuElement_2.style.color = "rgba(255, 255, 255, 0.3)";
+            break;
+        case 2:
+            ringButton_0.style.fillOpacity = "0.3";
+            ringButton_1.style.fillOpacity = "0.3";
+            ringButton_2.style.fillOpacity = "1";
+            menuElement_0.style.color = "rgba(255, 255, 255, 0.3)";
+            menuElement_1.style.color = "rgba(255, 255, 255, 0.3)";
+            menuElement_2.style.color = "#E3B873";
+            break;
+    }
+    return index = i;
+}    
+
+const prev = document.querySelectorAll(".main__secondpart_info-data_arrow_left");
+const next = document.querySelectorAll(".main__secondpart_info-data_arrow_right");
+
+prev[0].addEventListener('click', () => {           //вопрос: как тут обратиться ко всем элементам массива куда записались все элементы document.querySelectorAll
+    index = (index > 0) ? index - 1 : 2;
+    funcClick(index);
+})
+next[0].addEventListener('click', () => {
+    index = (index < 2) ? index + 1 : 0;
+    funcClick(index);
+})
